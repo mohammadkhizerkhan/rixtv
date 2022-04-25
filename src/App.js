0b4f62f5a88fcab94aab91c0180d8a198d43d65d
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2022-04-26 03:49:20 +0530

    added single video page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useLocation } from "react-router-dom";
 import "./App.css";
-import { Navbar, Sidebar,Categories,VideoListing } from "../src/components";
+import { Navbar, Sidebar,Categories,VideoListing,SingleVideo } from "../src/components";
 import {History, Home, Liked, Playlist, Watchlater} from "./pages";
+import { useEffect } from "react";
 function App() {
+  const location=useLocation();
+  useEffect(() => {
+    console.log(location)
+    console.log(window.history)
+  }, [location])
   return (
     <div className="App">
       <Navbar />
+      <div className="flex-row">
       <Sidebar />
       <Routes>
-        <Route path="/" element={<Home/>} >
-          <Route index element={<><Categories/><VideoListing/></>}/>
-          <Route path="/home" element={<><Categories/><VideoListing/></>}/>
+        <Route  element={<Home/>} >
+          {/* <Route index element={<><Categories/><VideoListing/></>}/> */}
+          <Route index path="/home" element={<><Categories/><VideoListing/></>}/>
+          <Route path="/video/:videoId" element={<SingleVideo/>}/>
           <Route path="/watchlater" element={<Watchlater/>}/>
           <Route path="/liked" element={<Liked/>}/>
           <Route path="/playlist" element={<Playlist/>}/>
           <Route path="/history" element={<History/>}/>
         </Route>
       </Routes>
+      </div>
     </div>
   );
 }
