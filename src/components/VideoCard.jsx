3c0d07baee2149fc5c0b78c132595f5f29259638
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2022-04-30 00:22:06 +0530

    added like feature in dropdown

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,7 +1,12 @@
-import React from "react";
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useAuth, useLike } from "../context";
+import { addToLike, removeFromLike } from "../services/LikedServices";
 
 function VideoCard({ video }) {
+  const [moreBtn, setMoreBtn] = useState(false);
+  const { token } = useAuth();
+  const { likeState, likeDispatch } = useLike();
   const navigate = useNavigate();
   const {
     _id,
@@ -15,7 +20,7 @@ function VideoCard({ video }) {
   } = video;
   return (
     <>
-      <div class="video-card">
+      <div class="video-card" onMouseLeave={() => setMoreBtn(false)}>
         <Link to={`/video/${_id}`}>
           <div className="thumbnail-div">
             <img
@@ -23,7 +28,8 @@ function VideoCard({ video }) {
               class="img-responsive video-img"
               alt=""
             />
-          <span class="video-time">09:30</span>
+
+            <span class="video-time">09:30</span>
           </div>
         </Link>
         <div class="video-card-details">
@@ -43,12 +49,41 @@ function VideoCard({ video }) {
               {views} views &nbsp; {uploaded}
             </div>
           </div>
-          <svg width="3rem" height="3rem" viewBox="0 0 24 24">
-            <path
-              fill="#d6d6d6"
-              d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2s-2 .9-2 2s.9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2z"
-            ></path>
-          </svg>
+          <div className="dropdown-label-div">
+            <button
+              className="btn btn-icon btn-4"
+              onClick={() => setMoreBtn(!moreBtn)}
+            >
+              <svg width="3rem" height="3rem" viewBox="0 0 24 24">
+                <path
+                  fill="#d6d6d6"
+                  d="M12 8c1.1 0 2-.9 2-2s-.9-2-2-2s-2 .9-2 2s.9 2 2 2zm0 2c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2zm0 6c-1.1 0-2 .9-2 2s.9 2 2 2s2-.9 2-2s-.9-2-2-2z"
+                ></path>
+              </svg>
+            </button>
+            {moreBtn && (
+              <div className="dropdown-label-btns">
+                {likeState.liked.some((like) => like._id === video._id) ? (
+                  <button
+                    className="btn btn-m dropdown-btn text-left font-bold font-15"
+                    onClick={() => removeFromLike(token, video, likeDispatch)}
+                  >
+                    Dislike
+                  </button>
+                ) : (
+                  <button
+                    className="btn btn-m dropdown-btn text-left font-bold font-15"
+                    onClick={() => addToLike(token, video, likeDispatch)}
+                  >
+                    Like
+                  </button>
+                )}
+                <button className="btn btn-m dropdown-btn text-left font-bold font-15">
+                  wathlater
+                </button>
+              </div>
+            )}
+          </div>
         </div>
       </div>
     </>
--- src/pages/Liked.jsx
+++ src/pages/Liked.jsx
@@ -2,36 +2,37 @@ import {useEffect} from 'react'
 import axios from "axios";
 import { useState } from 'react';
 import VideoCard from '../components/VideoCard';
-import { useAuth} from '../context'
+import { useAuth, useLike} from '../context'
 
 function Liked() {
     const [liked,setLiked]=useState([])
     const {token}=useAuth();
+    const {likeState}=useLike();
     
-    useEffect(() => {
-        (async ()=>{
-            try {
-                const {data} = await axios.get(
-                  "/api/user/likes",
-                  {
-                    headers: {
-                      authorization: token,
-                    },
-                  }
-                );
-                console.log(data)
-                setLiked(prev=>[...prev,...data.likes])
-              } catch (error) {
-                  console.log("error in get to LIKE",error)
-              }
-        })();
-    }, [])
+    // useEffect(() => {
+    //     (async ()=>{
+    //         try {
+    //             const {data} = await axios.get(
+    //               "/api/user/likes",
+    //               {
+    //                 headers: {
+    //                   authorization: token,
+    //                 },
+    //               }
+    //             );
+    //             console.log(data)
+    //             setLiked(prev=>[...prev,...data.likes])
+    //           } catch (error) {
+    //               console.log("error in get to LIKE",error)
+    //           }
+    //     })();
+    // }, [])
 
     return (
         <>
         <h1>this is Like page</h1>
         {
-            liked.map((video)=>{
+            likeState.liked.map((video)=>{
                 return (
                     <VideoCard key={video._id} video={video}/>
                 )
