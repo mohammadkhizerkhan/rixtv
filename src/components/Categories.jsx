9516a824d7c7c2beb7122d39869c834aca64aab7
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2022-05-04 23:20:12 +0530

    added category syling

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,12 +1,39 @@
-import React from "react";
+import axios from "axios";
+import {React,useEffect,useState} from "react";
 
 function Categories() {
+  const [categories, setCategories] = useState([])
+  useEffect(() => {
+    (
+      async ()=>{
+        try {
+          const res = await axios.get("/api/categories");
+          setCategories(res.data.categories);
+      } catch (error) {
+          console.error("error in get categories",error);
+      }
+      }
+    )();
+  }, [])
+  console.log(categories)
   return (
     <div class="main-categories">
-      <span class="category">All</span>
-      <span class="category">Live Stream</span>
-      <span class="category">GTA5</span>
-      <span class="category">PUBG</span>
+      {
+        categories.map(item=>{
+          return (
+            <div
+            className="input-wrapper color-white btn-category"
+        >
+            <label htmlFor={item._id} key={item._id}>
+              <input id={item._id} className="btn-tab-input" type="checkbox" key={item._id}/>
+              <span class="category">
+              {item.categoryName}
+              </span>
+            </label>
+        </div>
+          )
+        })
+      }
     </div>
   );
 }
--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,2 +1,14 @@
 import { ACTION_TYPE } from "../Action";
 
+const filterReducer=(state,action)=>{
+    switch (action.type) {
+        case "":
+            
+            break;
+    
+        default:
+            return state
+    }
+}
+
+export {filterReducer}
