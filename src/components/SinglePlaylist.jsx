5a8c2b038db05a701b019b6ce07a4c7dc5aab76b
mohammadkhizerkhan <64767673+mohammadkhizerkhan@users.noreply.github.com>
2022-05-15 01:01:34 +0530

    fixed dark mode in playlist

--- src/components/SinglePlaylist.jsx
+++ src/components/SinglePlaylist.jsx
@@ -13,7 +13,7 @@ function SinglePlaylist() {
     }, [])
     return (
         <>
-        {(playlistData?.videos?.length)?<h1>this is {playlistData?.title}</h1>:<h1>this playlist is empty</h1>}
+        {(playlistData?.videos?.length)?<h1 className='text-color text-center'>This is {playlistData?.title}</h1>:<h1>This playlist is empty</h1>}
             <section class="video-listing flex-row-wrap">
                 {
                     playlistData?.videos?.map(video=>{
